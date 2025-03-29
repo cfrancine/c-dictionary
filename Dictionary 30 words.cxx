b6b1593f224dd8ec8f97722a79a93a6a3160c87e
cfrancine <cfrancinelara@gmail.com>
2025-03-30 01:35:07 +0800

    Add files via upload

---
+++ Dictionary 30 words.cxx
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+	string search;
+	cout << "What to search?:";
+	cin >> search;
+	if (search == "Accumulator"){
+		cout << "A large rechargeable electric or a memory register used to contain the results of an arithmetical or logic operation";
+	}
+	else if (search == "Absorption_Wavemeter"){
+		cout << "A simple piece of equipment that used in measuring the energy of a specified radio frequency";
+	}
+	else if (search == "Balance"){
+		cout << "Adjusting the impedance of the circuit in order to achieve certain objective";
+	}
+	else if (search == "Base"){
+		cout << "In number system, Base is the number raised to the power which represents the number of units of a number system. In transistor, Base is a lead that is responsible for activating the transistor. By applying a small current at the base allows large current to flow from collector to emitter";
+	}
+	else if (search == "Cable"){
+		cout << "An insulated wire or a group of wires that are used in transmitting either electrical signals, or data or light waves";
+	}
+	else if (search == "Capacitor"){
+		cout << "A charge storage device, that has two conducting plates separated by a non conducting material";
+	}
+	else if (search == "Data Block"){
+		cout << "A data block is a sequence of data in bits or bytes that are usually transferred as a whole";
+	}
+	else if (search == "Drain"){
+		cout << "One of the three terminals of the MOSFET";
+	}
+	else if (search == "E-Core"){
+		cout << "E-core is a laminated configuration resembling the capital letter E in some transformers and inductive transducers";
+	}
+	else if (search == "Fading"){
+		cout << " In wireless communiction, fading is deviation of the attenuation affecting a signal over certain propagation media";
+	}
+	else if (search == "Gain"){
+		cout << "Gain is the capacity of a circuit to increase the strength of the input signal.The gain is measured by the ratio of the magnitude of the output signal to that of the input signal";
+	}
+	else if (search == "Hardware"){
+		cout << "A hardware is any physical component or assembled system that is meant to performed certain function in contrast to the software loaded";
+	}
+	else if (search == "Identity Law"){
+		cout << "Whereas elementary algebra applies to numbers Boolean algebra is about truth values true and false , or 1 and 0, and the resuts obtained by acting on them with AND,OR and NOT operators. Every digital computer uses Boolean algebra; all calculations are based on processing arrays of 1s and 0s";
+	}
+	else if (search == "Junction"){
+		cout << "Junction in which two materials featuring distinctly different electrical properties are in contact";
+	}
+	else if (search == "Kernal"){
+		cout << "Kernal is a program that constitutes the central core of a computer OS";
+	}
+	else if (search == "Latency"){
+		cout << "In programming, latency is a measure of time between the start of the tasks execution till the end of the task";
+	}
+	else if (search == "Magnetic Flux"){
+		cout << "Is a measure of strength of magnetic field passing through a given surface. The SI unit of magnetic flux is the Weber and CGS unit is Maxwell";
+	}
+	else if (search == "Nand Circuit"){
+		cout << "In a binary circuit, NAND circuit is a combination of NOT function and an AND funtion in which the output is FALSE if and only if both the inputs are false";
+	}
+	else if (search == "Object Code"){
+		cout << "Object code is produced by the compiler or interpreter which translates source code into object code. This object code is stored in object files and it is used as input to the linker";
+	}
+	else if (search == "Printed Circuits"){
+		cout << "An insulating board on which the conductive pattern of the circuit is etched from the layer of the board using materials such as copper, later the circuit components are placed";
+	}
+	else if (search == "Quantize"){
+		cout << "The process of sub dividing a continuous signal to discrete values";
+	}
+	else if (search == "Radar"){
+		cout << "A system that use synchronized radio transmitter and receiver to out pulse radio waves that can detect the presence, distance and activities of any distant objects like a space craft";
+	}
+	else if (search == "Sampling"){
+		cout << "Aprocess in which the input signal is converted into number of discrete electrical values either to store, process and/or display by an oscilloscope";
+	}
+	else if (search == "Tachometer"){
+		cout << "A device that measures the rotational speed especially the rotational speed of a shaft";
+	}
+	else if (search == "Underflow"){
+		cout << "In data processing, a condition arises when the computeed non-zero quantity in a machine is smaller than the smallest non-zero quantity that can be stored in the machine";
+	}
+	else if (search == "Varmeter"){
+		cout << "A test equipment that measures the reactive voltampere of a electrical circuit into which it is inserted";
+	}
+	else if (search == "Wave"){
+		cout << "A pattern of disturbance generated by the movement of energy propagating through a medium from one location to another";
+	}
+	else if (search == "X-Ray Emission"){
+		cout << "Emission of electromagnetic radiation of relatively high energy protons and with wavelength range of 0.01 nm to 10 nm's from a X-ray machine";
+	}
+	else if (search == "Y-Amplifier"){
+		cout << "Vertical amplifier of an oscilloscope is also called as Y-amplifier.";
+	}
+	else if (search == "Zip drive"){
+		cout << "A storage media similar to floppy disk with larger capacity than the conventional floppy disk ";
+		
+	}
+	return 0;
+}
